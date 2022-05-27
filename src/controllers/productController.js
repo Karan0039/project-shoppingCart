639432c0b55848f67d63e5830fc463a05697d6c8
AbiMangalan <86941986+AbiMangalan@users.noreply.github.com>
2022-05-27 11:07:06 +0530

    Abi M - updateProduct

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -128,6 +128,9 @@ const updateProduct = async function (req, res) {
         if (!isValidObjectId(productId))
             return res.status(400).send({ status: false, message: "The given productId is not a valid objectId" })
 
+        if(Object.keys(data).length==0||file.length==0)
+            return res.status(400).send({status: false, message: "Please provide product detail(s) to be updated."})
+
         let err = isInvalid(data, file)
         if (err) {
             error.push(...err)
